40166875fb7b7439614dd65abf73d2a08fdfc1bb
Zielinski.Kamil <215933@edu.p.lodz.pl>
2020-03-04 14:55:32 +0100

    nowa metoda policz2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code her
+        System.out.println("W drugim branchu; 2-3="+Policz2(2,3));
+    }
+
+    private static int Policz2(int i, int i1) {
+        return i-i1;
     }
 }
